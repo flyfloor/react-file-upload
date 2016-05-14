1c225c15357b683e6f59a9024b6a8d457f02f22a
jerryshew <jerryshew@gmail.com>
2016-05-15 00:58:57 +0800

    scratch

--- component/Uploader.js
+++ component/Uploader.js
@@ -1,63 +1,176 @@
 const React = require('react')
-
 const xhr = require('./xhr')
+
+const formatSize = (byte = 0) => {
+    byte = parseInt(byte)
+    if (byte / 1000000 > 1) {
+        return `${(parseInt(byte) / 1000000).toFixed(2)}m`
+    }
+    return `${(parseInt(byte) / 1000).toFixed(2)}k`
+}
 
 const Uploader = React.createClass({
     propTypes: {
         multiple: React.PropTypes.bool,
+        checkType: React.PropTypes.bool,
+        showDistroy: React.PropTypes.bool,
+        destroyIcon: React.PropTypes.element,
         url: React.PropTypes.string.isRequired,
+        mode: React.PropTypes.oneOf(['simple', 'list', 'card']),
     },
 
     getInitialState() {
         return {
-            list: [],
+            queue: [],
         };
     },
 
     getDefaultProps() {
         return {
             multiple: false,
+            mode: 'list',
             accept: "image/jpg,image/jpeg,image/gif,image/png,image/bmp,",
+            checkType: false,
+            showDistroy: true,
+            destroyIcon: <div>x</div>,
         };
     },
 
     handleFileChange(e){
-        this.getFiles(e.target.files, this.handleUpload);
+        this.handleFiles(e.target.files, this.handleUpload);
     },
 
-    getFiles(files, uploadHander){
-        for (let i = 0; i < files.length; i++) {
-            uploadHander(files[i]);
+    handleFiles(files, uploadFunc){
+        let {queue} = this.state
+        files = this.formatFiles(files)
+        this.setState({ 
+            queue: queue.concat(files)
+        })
+
+        Array.prototype.map.call(files, file => {
+            ((file) => {
+                file = this.initFileObj(file)
+                uploadFunc(file)
+            })(file)
+        })
+    },
+
+    formatFiles(files){
+        let rtn = []
+        let {accept, checkType} = this.props
+        Array.prototype.map.call(files, (file) => {
+            if (checkType && (!file.type || accept.indexOf(file.type) === -1)) {
+                console.error('文件类型不支持')
+                return
+            }
+            rtn.push(file)
+        })
+        return rtn
+    },
+
+    initFileObj(file){
+        let reader = new FileReader()
+        reader.onload = ((file) => {
+            return (e) => file.image = e.target.result
+        })(file)
+        reader.readAsDataURL(file)
+
+        if (file.completed === undefined) {
+            file.completed = 0
         }
+        return file
     },
 
-    handleUpload(file){
+    handleUpload(file) {
         let {url} = this.props
+        let that = this
         xhr({
-            url,
-            file,
+            url, file,
             onSuccess(data) {
-                console.log(data)
+                if (that.onSuccess) that.onSuccess(data)
             },
             onProgress(loaded, total){
-                console.log(loaded, total)
+                if (total) {
+                    let {queue} = that.state
+                    let index = queue.indexOf(file)
+                    if (index !== -1) {
+                        file.progress = loaded / total * 100
+                        that.setState({ queue })
+                    }
+                }
+            },
+            onError(error){
+                let {queue} = that.state
+                let index = queue.indexOf(file)
+                if (index !== -1) {
+                    delete queue[index]
+                    that.setState({ queue })
+                }
+                if (that.onError) that.onError(error)
+                console.error(error)
             }
         })
     },
 
+    handleDestroyItem(file){
+        let {queue} = this.state
+        let index = queue.indexOf(file)
+        if (index !== -1) {
+            delete queue[index]
+            this.setState({
+                queue
+            });
+        }
+    },
+
     render() {
-        const {multiple, accept} = this.props;
-        let displayNodes = this.state.list.map((url, index) => {
+        const {multiple, accept, mode, showDistroy, destroyIcon} = this.props
+        let {queue} = this.state
+        let displayNodes = queue.map((file, index) => {
+            let {progress, name, image, size} = file
             return (
-                <li className="_item" key={`img_${index}`}>
-                    <img src={url}/>
+                <li className={progress === 100 ? 'uploader-item completed' : 'uploader-item'} key={`img_${index}`}>
+                    <div className="uploader-preview">
+                        <img src={image}/>
+                    </div>
+                    {mode !== 'list'
+                        ? <div className='uploader-progress'>
+                                <div className="uploader-progress-bar" style={{'width': `${progress}%`}}></div>
+                            </div>
+                        : null }
+                    <div className="uploader-content">
+                        {mode === 'simple' 
+                            ? null
+                            : <div className="uploader-name">{name}</div> }
+                        {mode === 'list'
+                            ? <div className='uploader-progress'>
+                                <div className="uploader-progress-bar" style={{'width': `${progress}%`}}></div>
+                            </div>
+                            : null
+                        }
+                        {mode === 'simple' 
+                            ? null
+                            : <div className="uploader-size">
+                                <div>{formatSize(size)}</div>
+                            </div> }
+                        {showDistroy 
+                            ? <div className="uploader-destroy" onClick={() => this.handleDestroyItem(file)}>
+                                {destroyIcon}
+                            </div>
+                            : null}
+                    </div>
                 </li>
-            );
+            )
         })
+
         return (
-            <div>
-                <input type="file" multiple={multiple} accept={accept} onChange={this.handleFileChange}/>
-                <ul className="_list">
+            <div className='uploader'>
+                <label className="uploader-label">
+                    选择文件
+                    <input type="file" className="uploader-input" 
+                    multiple={multiple} accept={accept} onChange={this.handleFileChange}/>
+                </label>
+                <ul className={`uploader-queue ${mode}`}>
                     {displayNodes}
                 </ul>
             </div>
--- demo/demo.js
+++ demo/demo.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import uploadCss from './upload.less';
 import Css from './demo.less';
 import ReactDOM from 'react-dom';
 import Uploader from '../component/Uploader';
@@ -11,15 +12,21 @@ export class UploaderDemo extends Component {
                 <ul>
                     <li>
                         <h3>Default uploader</h3>
-                        <Uploader url='/upload'/>
+                        <Uploader url='/upload' checkType={true}/>
                     </li>
-                    {/*<li>
+                    <li>
                         <h3>Uploader support multiple file</h3>
-                        <Uploader multiple={true}/>
+                        <Uploader  url='/upload' multiple={true}/>
+                    </li>
+                    <li>
+                        <h3>Uploader mode</h3>
+                        <Uploader  url='/upload' mode={'card'}/>
+                        <h4>simple</h4>
+                        <Uploader  url='/upload' mode={'simple'}/>
                     </li>
                     <li>
                         <h3>Drag and drop uploader</h3>
-                    </li>*/}
+                    </li>
                 </ul>
             </div>
         );
